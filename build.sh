c6d768e8bcd271f749807b9b8c26d0b264adb970
Barrett Williamson <williamsonbarrett@gmail.com>
2025-05-16 21:37:25 -0400

    fix: dnf addrepo

--- build.sh
+++ build.sh
@@ -7,10 +7,8 @@ export MAJOR_VERSION_NUMBER
 
 dnf install -y tmux alacritty fastfetch fish
 
-dnf config-manager --add-repo "https://pkgs.tailscale.com/stable/rhel/${MAJOR_VERSION_NUMBER}/tailscale.repo"
-dnf config-manager --set-disabled tailscale-stable
-dnf -y --enablerepo tailscale-stable install \
-	tailscale
+dnf config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+dnf install tailscale
 
 dnf copr enable lilay/topgrade
 dnf install topgrade
