9e9126b1b07fb6fb04b2642c374a86adeb3da88d
Barrett <barrett@distant.social>
2025-05-18 00:02:02 -0400

    feat: add virt-viewer

--- build.sh
+++ build.sh
@@ -14,6 +14,7 @@ dnf install -y tmux \
         qemu \
         libvirt \
         virt-manager \
+        virt-viewer
 
 dnf config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
 dnf install -y tailscale
